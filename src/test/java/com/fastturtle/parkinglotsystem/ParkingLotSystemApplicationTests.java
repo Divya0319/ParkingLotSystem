de3f8a6b2d135b3666f809ba3d54bd5131b81377
Divya Gupta <divygupta0319@gmail.com>
2024-10-21 12:16:31 +0530

    Initial commit with project setup

---
+++ src/test/java/com/fastturtle/parkinglotsystem/ParkingLotSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastturtle.parkinglotsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
